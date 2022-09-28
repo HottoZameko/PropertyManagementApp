c2dc517729f209188c95b519d88384c1426944f8
Simbongile Zameko <s224432788@mandela.ac.za>
2022-09-29 01:40:50 +0200

    Additional ucReports Controls

--- PropertyManagementApp/ucReports.Designer.cs
+++ PropertyManagementApp/ucReports.Designer.cs
@@ -30,18 +30,25 @@ namespace PropertyManagementApp
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnTypesAndProperties = new System.Windows.Forms.GroupBox();
-            this.cmbUsers = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.btnSubProvCit = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnEndedRentals = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEndedRentals = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSubProvCit = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbUsers = new System.Windows.Forms.ComboBox();
+            this.btnTypesAndProperties = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            this.btnTypesAndProperties.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -64,85 +71,12 @@ namespace PropertyManagementApp
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Search Reports";
             // 
-            // btnTypesAndProperties
+            // textBox2
             // 
-            this.btnTypesAndProperties.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTypesAndProperties.Location = new System.Drawing.Point(443, 15);
-            this.btnTypesAndProperties.Name = "btnTypesAndProperties";
-            this.btnTypesAndProperties.Size = new System.Drawing.Size(529, 431);
-            this.btnTypesAndProperties.TabIndex = 1;
-            this.btnTypesAndProperties.TabStop = false;
-            this.btnTypesAndProperties.Text = "View Reports";
-            // 
-            // cmbUsers
-            // 
-            this.cmbUsers.FormattingEnabled = true;
-            this.cmbUsers.Location = new System.Drawing.Point(128, 44);
-            this.cmbUsers.Name = "cmbUsers";
-            this.cmbUsers.Size = new System.Drawing.Size(250, 29);
-            this.cmbUsers.TabIndex = 0;
-            this.cmbUsers.Text = "-Select User To View-";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 21);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "List Users:";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(243, 79);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 37);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "List Of Al Users";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(128, 135);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(276, 34);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "List Properties and Property Type";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // btnSubProvCit
-            // 
-            this.btnSubProvCit.Location = new System.Drawing.Point(128, 175);
-            this.btnSubProvCit.Name = "btnSubProvCit";
-            this.btnSubProvCit.Size = new System.Drawing.Size(276, 34);
-            this.btnSubProvCit.TabIndex = 4;
-            this.btnSubProvCit.Text = "List Cities, Provinces And Suburbs ";
-            this.btnSubProvCit.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 266);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 21);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Rentals From:";
-            // 
-            // btnEndedRentals
-            // 
-            this.btnEndedRentals.Location = new System.Drawing.Point(128, 215);
-            this.btnEndedRentals.Name = "btnEndedRentals";
-            this.btnEndedRentals.Size = new System.Drawing.Size(276, 32);
-            this.btnEndedRentals.TabIndex = 6;
-            this.btnEndedRentals.Text = "List Of Ended Rentals";
-            this.btnEndedRentals.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(125, 260);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 27);
-            this.textBox1.TabIndex = 7;
+            this.textBox2.Location = new System.Drawing.Point(269, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 27);
+            this.textBox2.TabIndex = 9;
             // 
             // label3
             // 
@@ -153,23 +87,129 @@ namespace PropertyManagementApp
             this.label3.TabIndex = 8;
             this.label3.Text = "To:";
             // 
-            // textBox2
+            // textBox1
             // 
-            this.textBox2.Location = new System.Drawing.Point(269, 257);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 27);
-            this.textBox2.TabIndex = 9;
+            this.textBox1.Location = new System.Drawing.Point(125, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 7;
+            // 
+            // btnEndedRentals
+            // 
+            this.btnEndedRentals.Location = new System.Drawing.Point(128, 215);
+            this.btnEndedRentals.Name = "btnEndedRentals";
+            this.btnEndedRentals.Size = new System.Drawing.Size(276, 32);
+            this.btnEndedRentals.TabIndex = 6;
+            this.btnEndedRentals.Text = "List Of Ended Rentals";
+            this.btnEndedRentals.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Rentals From:";
+            // 
+            // btnSubProvCit
+            // 
+            this.btnSubProvCit.Location = new System.Drawing.Point(128, 175);
+            this.btnSubProvCit.Name = "btnSubProvCit";
+            this.btnSubProvCit.Size = new System.Drawing.Size(276, 34);
+            this.btnSubProvCit.TabIndex = 4;
+            this.btnSubProvCit.Text = "List Cities, Provinces And Suburbs ";
+            this.btnSubProvCit.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(128, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(276, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "List Properties and Property Type";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "List Of Al Users";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "List Users:";
+            // 
+            // cmbUsers
+            // 
+            this.cmbUsers.FormattingEnabled = true;
+            this.cmbUsers.Location = new System.Drawing.Point(128, 44);
+            this.cmbUsers.Name = "cmbUsers";
+            this.cmbUsers.Size = new System.Drawing.Size(250, 29);
+            this.cmbUsers.TabIndex = 0;
+            this.cmbUsers.Text = "-Select User To View-";
+            // 
+            // btnTypesAndProperties
+            // 
+            this.btnTypesAndProperties.Controls.Add(this.dataGridView1);
+            this.btnTypesAndProperties.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTypesAndProperties.Location = new System.Drawing.Point(443, 15);
+            this.btnTypesAndProperties.Name = "btnTypesAndProperties";
+            this.btnTypesAndProperties.Size = new System.Drawing.Size(529, 209);
+            this.btnTypesAndProperties.TabIndex = 1;
+            this.btnTypesAndProperties.TabStop = false;
+            this.btnTypesAndProperties.Text = "View Reports";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(517, 177);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(443, 230);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(529, 216);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "View Reports";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 26);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(517, 184);
+            this.dataGridView2.TabIndex = 0;
             // 
             // ucReports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.btnTypesAndProperties);
             this.Controls.Add(this.groupBox1);
             this.Name = "ucReports";
             this.Size = new System.Drawing.Size(996, 460);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.btnTypesAndProperties.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -188,5 +228,8 @@ namespace PropertyManagementApp
         private System.Windows.Forms.Button btnEndedRentals;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSubProvCit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
